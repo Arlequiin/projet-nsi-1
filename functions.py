d61beabbbcb7eb23f66d6f89728c5ee4c67e379c
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2022-10-25 23:38:23 +0100

    finished cesar

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 def error(n):
-    errors={
-    0:"Problème technique...\nCela ne peut être du qu'à une erreur de programmation.",
-    1:"/!\ Erreur, vous devez choisir un nombre entre un et deux. Veuillez réessayer.",
-    2:"/!\ Erreur, votre texte ne doit contenir que des lettres."
-    }
-    print(colored("Erreur, vous devez choisir un nombre entre un et deux. Veuillez réessayer.",255,0,0))
-def colored(text,r=255,g=255,b=255):
+    errors=["exception non gérée.",
+    "vous devez choisir un nombre entre un et deux. Veuillez réessayer.",
+    "Erreur, votre texte ne doit contenir que des lettres.",
+    "Aucun fichier 'cesar.txt' dans le dossier."
+    ]
+    print(colored("/!\ Erreur, "+errors[n],255,0,0))
+def colored(text,r=200,g=200,b=200):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r,g,b,text)
--- main.py
+++ main.py
@@ -6,37 +6,65 @@ def to_list_of_indexes(charset):
    charset=charset.lower()
    list_of_indexes=[]
    for char in charset:
-      list_of_indexes.append(alphabet.index(char))
+      if char in alphabet:
+       list_of_indexes.append(alphabet.index(char))
+      else:
+         list_of_indexes.append(char)
    return list_of_indexes
 def encode(charset,cesar_degree=3):
    indexes=to_list_of_indexes(charset)
    output=[]
    for index in indexes:
-      output.append(alphabet[index+cesar_degree])
+    if type(index)==int:
+      diff=index+cesar_degree
+      if diff>len(alphabet)-1:
+         diff=0+(diff-(len(alphabet)))
+      output.append(alphabet[diff])
+    else:
+      output.append(index)
    output=''.join(output).capitalize()
    return output
-a=input("βοnjοur, quε sοuηαιτεz-vous fαιre?\n 1. ΔΕCΟΔΕ / 2. ΕΝCΟΔΕ\n>>> ")
+def decode(charset,cesar_degree=-3):
+   return encode(charset,cesar_degree)
+a=input("βοnjοur, quε sοuηαιτεz-vous fαιre?\n 1. ΕΝCΟΔΕ/ 2. ΔΕCΟΔΕ\n>>> ")
 while True:
  try:
     operation_choice={'1':'encode','2':'decode'}[a]
     break
  except:
     error(1)
-    a=input("1. ΔΕCΟΔΕ / 2. ΕΝCΟΔΕ\n>>> ")
+    a=input("1. ΕΝCΟΔΕ / 2. ΔΕCΟΔΕ\n>>> ")
 print(f"Comment voulez-vous {operation_choice}r ?")
 while True:
  try:
-  type_choice={'1':'input','2':'choice'}[input(f"1. Σαιsιr du τεχτε / 2. ficηιεr\n>>> ")]
+  type_choice={'1':'input','2':'file'}[input(f"1. Σαιsιr du τεχτε / 2. ficηιεr\n>>> ")]
   break
  except:
    error(1)
 if type_choice=='input':
- base_text=input(colored(f"Saisissez le texte à {operation_choice} 🔒\n>>> ",0,0,200))
+ print(colored(f"Saisissez le texte à {operation_choice} 🔒",0,0,200))
  if operation_choice=='encode':
    while True:
     try:
      print(encode(input(">>> ")))
      break
     except:
-      error(2)
-print(colored("Fin de l'exécution",0,0,255))
+      error(0)
+ elif operation_choice=="decode":
+   while True:
+    try:
+     print(decode(input(">>> ")))
+     break
+    except:
+      error(0)
+elif type_choice=='file':
+   input(colored("Nommez votre fichier 'cesar.txt' et mettez-le dans le dossier 'projet-nsi-1', appuyez ensuite sur Entrée.",0,0,200))
+   try:
+      with open('cesar.txt','r') as f:
+         content=f.read()
+      with open('cesar.txt','w') as f2:
+         f2.write(eval(operation_choice+"(content)"))
+   except:
+      error(0)
+   print(colored("Votre fichier a été déchiffré avec succès!",0,100,150))
+print(colored("Fin de l'exécution",0,255,0))
