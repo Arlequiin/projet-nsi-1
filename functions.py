84530f28e0fa109750daf76f4690d0683d7b019a
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2022-10-26 10:59:17 +0100

    pendu sans ui final

--- functions.py
+++ functions.py
@@ -4,7 +4,8 @@ def error(n):
     errors=["exception non gérée.",
     "vous devez choisir un nombre entre un et deux. Veuillez réessayer.",
     "Erreur, votre texte ne doit contenir que des lettres.",
-    "Aucun fichier 'cesar.txt' dans le dossier."
+    "Aucun fichier 'cesar.txt' dans le dossier.",
+    "Ce n'est pas une lettre."
     ]
     print(colored("/!\ Erreur, "+errors[n],255,0,0))
 def colored(text,r=200,g=200,b=200):
